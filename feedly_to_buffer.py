d9eff1aea39342f36c048a72bde99b8b40db1be5
Matt Reynolds <matt@mdreynolds.co.uk>
2019-05-05 08:46:37 +0100

    Update feedly_to_buffer.py

--- feedly_to_buffer.py
+++ feedly_to_buffer.py
@@ -3,8 +3,8 @@ import json
 import re
 
 #FEEDLY REQUESTS:
-user_id = '3a01f33e-d2f7-4cfe-9a2f-61f602a305fa'
-access_token = 'A3aWNtW9aW5wRELjxNyJDscXzlGh6FNqEFHPNbfJICJHkRcu72g9I2GSaN8zveI2Elbgn6tm4IL4XUqOZ55XWouenDIJOThrxRgcQqZQcjLXeT-F5nx4l4hMvE7y1BR8X_n4Aht4BikdsLqe1q7bOx17aIuV6uY9YAOakHUKQNlA86bipfb_l14k6TCD_j06HyveGr4PxESornr1V_YtRgGCyqo2ASaSLqcLFmolKZhw_RxanilQsZdeqMiZkEo:feedlydev'
+user_id = ##user-id (optional)
+access_token = ##your-access-token
 tags_url = 'https://cloud.feedly.com/v3/tags'
 streams_url = 'https://cloud.feedly.com/v3/streams/contents?streamId='
 markers_url = 'https://cloud.feedly.com/v3/markers'
@@ -50,9 +50,9 @@ def construct_articles():
 
 #BUFFER POSTS:
 
-buffer_client_id = '5cc5ade2c2de1b657a081858'
-buffer_access_token = '1/a54ba90e93b1ea8d54c460eb179a497c'
-buffer_client_secret = '0dad187436b977f44068a61dffc6a16c'
+buffer_client_id = ##your-id (optional)
+buffer_access_token = ##your-access-token (required)
+buffer_client_secret = ##your-client-secret (optional)
 buffer_redirect_url = 'urn:ietf:wg:oauth:2.0:oob'
 buff_header = {'Authorization': buffer_access_token}
 buffer_profile_url = 'https://api.bufferapp.com/1/profiles.json'
@@ -70,23 +70,6 @@ def buff_ids():
     return int(ids)
 
 
-#POST NEW ARTICLES TO BUFFER ACCOUNT:
-
-#SET DATA TO BE POSTED:
-#def buff_data_set():
-#    buff_data_update = {
-#        "profile_ids": ["{}".format(buff_profile[ids]['id'])],
-#        "text": "This is an update example...",
-#        "media": {
-#            "link": "http://google.com",
-#            "title": "Google",
-#            "description": "The google homepage",
-#            "picture": None,
-#            "photo": None,
-#            "thumbnail": None,
-#        },
-#
-#    }
 
 
 #RUN UPLOAD TO BUFFER:
@@ -109,25 +92,6 @@ def buff_post():
         requests.post('{}?access_token={}'.format(buffer_create_url, buffer_access_token), data=(buff_data_update))
 
 
-
-#requests.post('https://api.bufferapp.com/1/updates/create.json?access_token=1/a54ba90e93b1ea8d54c460eb179a497c', data=(buff_data_update))
-
-#buff_post()
-#POST Data
-#text=This%20is%20an%20example%20update&
-#profile_ids[]=4eb854340acb04e870000010&
-#profile_ids[]=4eb9276e0acb04bb81000067&
-#media[link]=http%3A%2F%2Fgoogle.com&
-#media[description]=The%20google%20homepage
-
-
-#print(buff_ids())
-
-#print(buff_profile)
-
-#RUN WHOLE PROCESS:
-
-#def start_run():
 
 
 
